3825a25a8f2ed492b4d7a40d54a32e60247e1d29
nueko <nueko@yahoo.com>
2017-05-22 18:34:16 +0700

    quick fix postinstall script

--- bin/postinstall.ps1
+++ bin/postinstall.ps1
@@ -1,19 +1,11 @@
-switch ($args[0]) { 
-    53 {"post_install script for php 53 not yet implemented."} 
-    54 {"post_install script for php 54 not yet implemented."} 
-    55 {"post_install script for php 55 not yet implemented."} 
-    56 {"post_install script for php 56 not yet implemented."} 
-    70 {"post_install script for php 70 not yet implemented."} 
-    71 {
-        # Create directory for custom PHP configuration
-        $confd = "$dir\conf.d"
-        if (!(Test-Path $confd)) {
-            (New-Item -Type directory $confd) | Out-Null
-        }
+# Create directory for custom PHP configuration
+$confd = "$dir\conf.d"
 
-        # (Get-Content "$dir\php.ini-development") | Where-Object { $_ -notmatch '^;' }  | ? {$_.trim() -ne '' } | Set-Content "$persist_dir\development.ini"
-
-        # Enable extensions to be found in installation-relative folder (the default is to search C:/php)
-        (Get-Content "$dir\php.ini-production") | ForEach-Object { $_ -replace '; extension_dir = "ext"', 'extension_dir = "ext"' } | Set-Content "$dir\php.ini"
-    } 
+if (!(Test-Path $confd)) {
+    (New-Item -Type directory $confd) | Out-Null
 }
+
+# (Get-Content "$dir\php.ini-development") | Where-Object { $_ -notmatch '^;' }  | ? {$_.trim() -ne '' } | Set-Content "$persist_dir\development.ini"
+
+# Enable extensions to be found in installation-relative folder (the default is to search C:/php)
+(Get-Content "$dir\php.ini-production") | ForEach-Object { $_ -replace '; extension_dir = "ext"', 'extension_dir = "ext"' } | Set-Content "$dir\php.ini"
