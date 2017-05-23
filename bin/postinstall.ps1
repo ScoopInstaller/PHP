9e8200cfde9a5e0f81bcd55a0f02a15dca22c77a
nueko <nueko@yahoo.com>
2017-05-24 03:32:51 +0700

    tweak postinstall

--- bin/postinstall.ps1
+++ bin/postinstall.ps1
@@ -6,9 +6,14 @@ if (!(Test-Path $confd)) {
 }
 
 # (Get-Content "$dir\php.ini-development") | Where-Object { $_ -notmatch '^;' }  | ? {$_.trim() -ne '' } | Set-Content "$persist_dir\development.ini"
-$base_ini = "$dir\php.ini-production";
+$prod_ini = "$dir\php.ini-production"
 if(!(Test-Path "$dir\php.ini-production")) {
-    $base_ini = "$dir\php.ini-dist"
+    $prod_ini = "$dir\php.ini-dist"
 }
+
 # Enable extensions to be found in installation-relative folder (the default is to search C:/php)
-(Get-Content "$base_ini") | ForEach-Object { $_ -replace '; extension_dir = "ext"', 'extension_dir = "ext"' } | Set-Content "$dir\php.ini"
+if(Test-Path $prod_ini) {
+    (Get-Content "$prod_ini") | ForEach-Object { $_ -replace '; extension_dir = "ext"', 'extension_dir = "ext"' } | Set-Content "$dir\php.ini"
+}
+
+Copy-Item -ErrorAction Ignore "$dir\php.ini-development" $persist_dir
