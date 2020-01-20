5b3a98e01ba2c49c8294285ef772d72140ecb6d1
Jakub Čábera <cabera.jakub@gmail.com>
2020-01-20 09:51:02 +0100

    remove debug

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,4 +1,4 @@
-if(!$env:SCOOP_HOME) { write-host 'ANO'; $env:SCOOP_HOME = scoop prefix scoop | Resolve-Path }
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = scoop prefix scoop | Resolve-Path }
 $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 $dir = "$psscriptroot/../bucket" # checks the parent dir
 Invoke-Expression -Command "$checkver -dir $dir $($args | ForEach-Object { "$_ " })"
